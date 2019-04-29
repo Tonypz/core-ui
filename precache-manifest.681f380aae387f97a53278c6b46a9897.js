a29926aadd05fde241840e6674eb4b07ac5ff955
Travis-CI <comindware-awesome-b@comindware.com>
2019-04-29 10:39:39 +0000

    Auto-deploy to Github Pages

---
+++ precache-manifest.681f380aae387f97a53278c6b46a9897.js
@@ -0,0 +1,53 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0ebeda6fa507eefdc6",
+    "url": "app.js"
+  },
+  {
+    "url": "icon_128x128.714d63aeda32d131b01d0ef137fd68a1.png"
+  },
+  {
+    "url": "icon_144x144.1a171c39c07bf6b29d652e00a46adc2d.png"
+  },
+  {
+    "url": "icon_152x152.b2666e72872ff41915d0a403f7d64ecf.png"
+  },
+  {
+    "url": "icon_192x192.3420fe48c7b98b3e48be6ef2d8058cd0.png"
+  },
+  {
+    "url": "icon_384x384.7b318db198bec11c4586efb16efe2928.png"
+  },
+  {
+    "url": "icon_512x512.fcff4271d5ae3728a9de51fcd51f1bc8.png"
+  },
+  {
+    "url": "icon_72x72.120a434576809a7246b0e3370eef5f3f.png"
+  },
+  {
+    "url": "icon_96x96.7aff45e523c76f5d83459fe6ebd25fe6.png"
+  },
+  {
+    "url": "manifest.734dbca253a12f888fe913f16e11fb50.json"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.eot"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.svg"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2"
+  }
+]);
